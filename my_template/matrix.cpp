637634c59c7128ecd8e6c015b4ae12f5314f866b
Adarsh Kumar <adarshkr532@gmail.com>
2018-11-26 19:50:21 +0530

    Create matrix.cpp

---
+++ my_template/matrix.cpp
@@ -0,0 +1,71 @@
+int sz;
+const int NN = 101;
+
+class matrix{
+public:
+    ll mat[NN][NN];
+    matrix(){
+        for(int i = 0; i < NN; i++)
+            for(int j = 0; j < NN; j++)
+                mat[i][j] = 0;
+        sz = NN;
+    }
+    inline matrix operator * (const matrix &a){
+        matrix temp;
+        for(int i = 0; i < sz; i++)
+            for(int j = 0; j < sz; j++){
+                for(int k = 0; k < sz; k++){
+                temp.mat[i][j] += (mat[i][k] * a.mat[k][j]) % mod;
+                if(temp.mat[i][j] >= mod)
+                    temp.mat[i][j] -= mod;   
+            }
+        }
+        return temp;
+    }
+    inline matrix operator + (const matrix &a){
+        matrix temp;
+        for(int i = 0; i < sz; i++)
+            for(int j = 0; j < sz; j++){
+                temp.mat[i][j] = mat[i][j] + a.mat[i][j] ;
+                if(temp.mat[i][j] >= mod)
+                    temp.mat[i][j] -= mod;
+            }
+        return temp;
+    }
+    inline matrix operator - (const matrix &a){
+        matrix temp;
+        for(int i = 0; i < sz; i++)
+            for(int j = 0; j < sz; j++){
+                temp.mat[i][j] = mat[i][j] - a.mat[i][j] ;
+                if(temp.mat[i][j] < mod)
+                    temp.mat[i][j] += mod;
+            }
+        return temp;
+    }
+    inline void operator = (const matrix &b){
+        for(int i = 0; i < sz; i++)
+            for(int j = 0; j < sz; j++)
+                mat[i][j] = b.mat[i][j];
+    }
+    inline void print(){
+        for(int i = 0; i < sz; i++){
+            for(int j = 0; j < sz; j++){
+                cout << mat[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+matrix pow(matrix a, ll k){
+    matrix ans;
+    for(int i = 0; i < sz; i++)
+        ans.mat[i][i] = 1;
+    while(k){
+        if(k & 1)
+            ans = ans * a;
+        a = a * a;
+        k >>= 1;
+    }
+    return ans;
+}

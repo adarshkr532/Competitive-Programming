5881b4221d179f7ec1cadb7e6d5baf4483b3b7bc
Adarsh Kumar <adarshkr532@gmail.com>
2019-12-07 10:30:08 +0530

    update scc.cpp

--- my_template/SCC.cpp
+++ my_template/SCC.cpp
@@ -1,34 +1,41 @@
-vector<int> g[N], rg[N];
+int comp[N];
 stack<int> st;
 bool vis[N];
-int n, comp[N];
-
-void dfs1(int u){
+vector<int> g[N], rg[N];
+ 
+void add(int u, int v){
+	g[u].push_back(v);
+	rg[v].push_back(u);
+}
+ 
+void dfs(int u){
 	vis[u] = 1;
 	for(auto i: g[u]){
-		if(vis[i])	continue;
-		dfs1(i);
+		if(!vis[i])
+			dfs(i);
 	}
 	st.push(u);
 }
-
-void dfs2(int u, int k){
-	comp[u] = k; 
+ 
+void dfs1(int u, int cur){
+	comp[u] = cur;
 	for(auto i: rg[u]){
-		if(comp[i])	continue;
-		dfs2(i, k);
+		if(!comp[i])
+			dfs1(i, cur);
 	}
 }
-
-void scc(){
+ 
+void scc(int n){
 	for(int i = 1; i <= n; i++){
-		if(!vis[i])	dfs1(i);
+		if(!vis[i])	dfs(i);
 	}
 	int grp = 0;
 	while(!st.empty()){
 		int x = st.top();
 		st.pop();
-		if(comp[x])	continue;
-		dfs2(x, ++grp);	
+		if(comp[x] == 0){
+			grp++;
+			dfs1(x, grp);
+		}
 	}
 }

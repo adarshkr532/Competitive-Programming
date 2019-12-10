7ecc41f0183e0d7b7f3b29627b43e13274a5420b
Adarsh Kumar <adarshkr532@gmail.com>
2019-12-11 00:47:36 +0530

    Update SuffixArray.cpp
    Added lcp array

--- my_template/SuffixArray.cpp
+++ my_template/SuffixArray.cpp
@@ -51,3 +51,26 @@ vector<int> suffix_array_construction(string s) {
     sorted_shifts.erase(sorted_shifts.begin());
     return sorted_shifts;
 }
+
+vector<int> lcp_construction(string const& s, vector<int> const& p) {
+    int n = s.size();
+    vector<int> rank(n, 0);
+    for (int i = 0; i < n; i++)
+        rank[p[i]] = i;
+
+    int k = 0;
+    vector<int> lcp(n-1, 0);
+    for (int i = 0; i < n; i++) {
+        if (rank[i] == n - 1) {
+            k = 0;
+            continue;
+        }
+        int j = p[rank[i] + 1];
+        while (i + k < n && j + k < n && s[i+k] == s[j+k])
+            k++;
+        lcp[rank[i]] = k;
+        if (k)
+            k--;
+    }
+    return lcp;
+}
